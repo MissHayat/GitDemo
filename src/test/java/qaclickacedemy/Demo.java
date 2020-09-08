6a9961793e14e9ab4d0ee4670fe4dfabd35cc7f0
hayat@gmail.com <=misshayatk96@gmail.com>
2020-09-08 16:08:22 +0530

    Added Demo

--- src/test/java/qaclickacedemy/Demo.java
+++ src/test/java/qaclickacedemy/Demo.java
@@ -1,5 +1,15 @@
 package qaclickacedemy;
 
+import org.testng.annotations.Test;
+
 public class Demo {
 
+	@Test
+	public void DemoAutomate()
+	{
+		System.out.println("DemoAutomate");
+		
+		System.out.println("DemoAutomate");
+	}
+	
 }
