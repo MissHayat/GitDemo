988befe991b7953bd12a3a030315429db54b6e97
hayat@gmail.com <=misshayatk96@gmail.com>
2020-09-08 14:17:02 +0530

    asian persn

--- src/test/java/qaclickacedemy/AppiumTest.java
+++ src/test/java/qaclickacedemy/AppiumTest.java
@@ -1,12 +1,18 @@
-package qaclickacedemy;
+package GitDemo.src.test.java.qaclickacedemy;
 
 import org.testng.annotations.Test;
+import org.testng.annotations.Test;
+import java.lang;
 
 public class AppiumTest {
 	@Test
 	public void NativeAppAndroid()
 	{
 		System.out.println("NativeAppAndroid");
+		
+		System.out.println("HybridAppAndroid");
+		
+		System.out.println("AppAndroid");
 	}
 
 	@Test
